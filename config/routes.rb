57c29f8479fc780341f07182faf5af702e9dd31d
brunocordeiro <brunocordeiro180@hotmail.com>
2017-06-10 00:24:14 -0300

    Welcome criado

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :itinerario_metros
   resources :itinerarios
   resources :estacaos
@@ -14,5 +16,7 @@ Rails.application.routes.draw do
   resources :cidades
   resources :linhas
   resources :usuarios
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
